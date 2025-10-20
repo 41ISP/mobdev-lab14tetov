6eedb866b92222c2ed043269c5e048ba0499bded
Andrey <andrey.28.06@mail.ru>
2025-10-20 08:05:07 +0000

    skra

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+ plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            devOptions: {
+                enabled: true,
+            },
+            manifestFilename: 'manifest.json',
+            includeManifestIcons: false,
+        }),
+    ],
 })
